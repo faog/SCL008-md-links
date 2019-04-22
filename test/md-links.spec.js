937401994637a7a8426906a5832c8a9e93f72718
faog <fabiola.orellana.g@gmail.com>
2019-04-22 17:11:20 -0400

    Corrección en el test que elimina el warning

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -2,12 +2,12 @@ const mdLinks = require('../md-links');
 
 describe('mdLinks', () => {
 
-  it('Debería retornar 2 links para el archivo prueba.md', () => {
+  it('Debería retornar 2 links para el archivo prueba.md', () => {    
     expect(mdLinks.mdLinks('./prueba.md')).resolves.toEqual(['https://es.wikipedia.org/wiki/Markdown','https://nodejs.org/']);
   });
 
-  it('Debería retornar error para el archivo prueba2.md', () => {
-    expect(mdLinks.mdLinks('./prueba2.md')).rejects.toEqual( {errno: -4058});
+  it('Debería retornar error para el archivo prueba2.md', async()  => {
+    await expect(mdLinks.mdLinks('./prueba2.md')).rejects.toThrow("ENOENT: no such file or directory, open './prueba2.md'");
   });
 
 });
