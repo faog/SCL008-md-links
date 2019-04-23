0e4b6c94d40d7b6a3b4781d2a96d937ceb861b88
faog <fabiola.orellana.g@gmail.com>
2019-04-23 08:59:21 -0400

    Se agrega un tercer test, para cuando se quiera leer un archivo con extensión distinta a .md

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -4,16 +4,16 @@ describe('mdLinks', () => {
 
   it('Debería retornar 2 links para el archivo prueba.md', () => {    
     expect(mdLinks.extractLinksFromFile('./prueba.md')).resolves.toEqual(
-    [ { href: 'https://es.wikipedia.org/wiki/Markdown',
-    text: 'Markdown',
-    file: './prueba.md' },
-    { href: 'https://nodejs.org/',
-    text: 'Node.js',
-    file: './prueba.md' }]);
+    [{href:'https://es.wikipedia.org/wiki/Markdown', text:'Markdown', file:'./prueba.md' },
+    {href: 'https://nodejs.org/', text:'Node.js', file:'./prueba.md'}]);
   });
 
   it('Debería retornar error para el archivo prueba2.md', async()  => {
     await expect(mdLinks.extractLinksFromFile('./prueba2.md')).rejects.toThrow("ENOENT: no such file or directory, open './prueba2.md'");
+  });
+
+  it('Debería retornar "Extension no válida" para el archivo text.txt', async()  => {
+    await expect(mdLinks.extractLinksFromFile('./text.txt')).rejects.toThrow("Extensión no válida");
   });
 
 });
