67e9963fc6f88a0bc3fe3a4b570bd061a89b3079
faog <fabiola.orellana.g@gmail.com>
2019-04-24 15:22:21 -0400

    Mejora en el codigo de la funcion extractLinksFromFile

--- index.js
+++ index.js
@@ -3,15 +3,14 @@
 const mdLinks = require("./md-links");
 
 mdLinks.extractLinksFromFile(process.argv[2])
-.then((links)=>{
-  links.forEach(function (link) {
+  .then((links)=>{
+    links.forEach(function (link) {
     /*Impresion de los links de acuerdo al formato solicitado, ademas que el texto no es superior a 
     50 carácteres*/
     console.log(`${link.file} ${link.href} ${link.text.substring(0,50)}`);
-  }); 
-  
-})
-.catch(console.error);
+    }); 
+  })
+  .catch(console.error);
 /*
 mdLinks("./some/example.md", { validate: true })
   .then(links => {
