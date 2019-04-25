0fb3bf75b136037cd9c45e7dc7523d7729063a40
faog <fabiola.orellana.g@gmail.com>
2019-04-25 10:26:13 -0400

    llamado a la promesa que permite validar links

--- index.js
+++ index.js
@@ -3,14 +3,45 @@
 const mdLinks = require("./md-links");
 
 mdLinks.extractLinksFromFile(process.argv[2])
-  .then((links)=>{
-    links.forEach(function (link) {
+  .then((res)=>{
+    res.forEach(function (link) {
     /*Impresion de los links de acuerdo al formato solicitado, ademas que el texto no es superior a 
     50 carácteres*/
     console.log(`${link.file} ${link.href} ${link.text.substring(0,50)}`);
     }); 
   })
   .catch(console.error);
+
+
+
+let url = "https://www.google.com";
+mdLinks.validateLink(url)
+  .then(res => {
+    console.log(res);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+
+
+// mdLinks.validateLink(links)
+//   .then((res)=>{
+//     console.log(res);
+//     mdLinks.extractLinksFromFile(res)
+//       .then(res => {
+//         console.log(res)
+//       })
+//       .catch(err => {
+//         console.log(err);
+//    });
+//    mdLinks.extractLinksFromFile(res);
+//  })
+//  .then((res)=>{  
+//  })
+//  .catch((err)=>{
+//    console.log(err);
+//  })
 /*
 mdLinks("./some/example.md", { validate: true })
   .then(links => {
