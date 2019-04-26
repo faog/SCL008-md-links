73a0deac340a08f03d2cc95c1e3623c695c4e72d
faog <fabiola.orellana.g@gmail.com>
2019-04-26 14:40:41 -0400

    Creación de test para --validate

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -15,5 +15,17 @@ describe('mdLinks', () => {
   it('Debería retornar "Extension no válida" para el archivo text.txt', async()  => {
     await expect(mdLinks.mdLinks('./text.txt')).rejects.toThrow("Extensión no válida");
   });
+
+  it('Debería retornar 2 links para el archivo prueba.md validando su status y statusText', async () => {
+    await expect(mdLinks.mdLinks('./prueba.md',{validate:true})).resolves.toEqual(
+    [{href:'https://es.wikipedia.org/wiki/Markdown', text:'Markdown', file:'./prueba.md',status:200,statusText:'OK' },
+    {href: 'https://nodejs.org/', text:'Node.js', file:'./prueba.md',status:200,statusText:'OK'}]);
+  });
+
+  it('Debería retornar un error al chequear un link no válido', async()=>{
+    await expect(mdLinks.mdLinks([{href:'https://esfffffff.wikipedia.org/wiki/Markdown', 
+    text:'Markdown', file:'./prueba.md'}])).rejects.toThrow("The \"path\" argument must be of type string. Received type object");
+  });
+  
 });
 
