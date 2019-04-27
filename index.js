7dfa829ee9a55129d35d602c1e784c1239fce042
faog <fabiola.orellana.g@gmail.com>
2019-04-27 15:58:30 -0400

    llamado de la función stats, cuando el usuario ingresa por CLI --stats --validate

--- index.js
+++ index.js
@@ -2,31 +2,43 @@
 
 const mdLinks = require("./src/md-links");
 
-if(process.argv[3]==="--validate"){
+if((process.argv[3]==="--validate" && process.argv[4]==="--stats") || 
+  (process.argv[3]==="--stats" && process.argv[4]==="--validate")){
+  mdLinks.mdLinks(process.argv[2], {validate: true})
+    .then((links)=>{
+      let responseStats=mdLinks.statsLinks(links, {validate: true});
+      console.log(`Total: ${responseStats.linksTotal}`);
+      console.log(`Unique: ${responseStats.linksUnique}`);
+      console.log(`Broken: ${responseStats.linksBroken}`)
+    })
+    .catch(console.error);
+  }
+else if(process.argv[3]==="--validate"){
   mdLinks.mdLinks(process.argv[2],{validate:true})
-  .then((links) => {
-    links.forEach(function (link) {   
-      console.log(`${link.file} ${link.href} ${link.status} ${link.statusText} ${link.text.substring(0,50)}`);
-    }); 
-  })
-  .catch(console.error);
+    .then((links) => {
+      links.forEach(function (link) {   
+        console.log(`${link.file} ${link.href} ${link.status} ${link.statusText} ${link.text.substring(0,50)}`);
+      }); 
+    })
+    .catch(console.error);
 }else if(process.argv[3]==="--stats"){
   mdLinks.mdLinks(process.argv[2])
-  .then((links) => {
-    let responseStats=mdLinks.statsLinks(links);
-    console.log(`Total: ${responseStats.linksTotal}`);
-    console.log(`Unique: ${responseStats.linksUnique}`);
-  })
-  .catch(console.error);
+    .then((links) => {
+      let responseStats=mdLinks.statsLinks(links);
+      console.log(`Total: ${responseStats.linksTotal}`);
+      console.log(`Unique: ${responseStats.linksUnique}`);
+    })
+    .catch(console.error);
+
 
 }else {
   mdLinks.mdLinks(process.argv[2])
-  .then((links) => {
-    links.forEach(function (link) {   
-      console.log(`${link.file} ${link.href} ${link.text.substring(0,50)}`);
-    }); 
-  })
-  .catch(console.error);
+    .then((links) => {
+      links.forEach(function (link) {   
+        console.log(`${link.file} ${link.href} ${link.text.substring(0,50)}`);
+      }); 
+    })
+    .catch(console.error);
 }
 
 /*
