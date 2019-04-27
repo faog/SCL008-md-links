f047727dc624bcfc2f2c0af8cfb7181ff35190cf
faog <fabiola.orellana.g@gmail.com>
2019-04-27 15:57:04 -0400

    modificación de la función stats, agregando la opción para que usuario puede identificar los link rotos

--- src/md-links.js
+++ src/md-links.js
@@ -5,11 +5,10 @@ const marked = require('marked');
 const fetch = require('node-fetch');
 
 /*Función mdLinks 
-- Permite la conexión entre las funciones contruidas
+- Permite la conexión entre las funciones construidas
 - Permite la interacción entre index.js y md-links.js*/
-
-const mdLinks = (path,option) => {
-    if(option && option.validate){
+const mdLinks = (path,options) => {
+    if(options && options.validate){
         return new Promise((resolve,reject)=>{
             extractLinksFromFile(path).then((links)=>{
                 resolve(validateLink(links));
@@ -25,6 +24,7 @@ const mdLinks = (path,option) => {
 1) Función extractLinksFromFile que permite extraer los links de un archivo .md
 PASOS: 
 - Se lee el contenido del archivo con nombre "path" y se guarda como String en la variable markdown
+- Se valida que la extensión del archivo sea .md
 - El renderer que se crea en vez de convertir los links a html, va a ejecutar lo que sigue cada vez que encuentre un link
 - Se ejecuta la conversión indicando que en vez del renderer que viene por omisión 
 que convierte todo a HTML), se usará el renderer anteriormente creado que ingresó los links en el arreglo. 
@@ -45,7 +45,7 @@ const extractLinksFromFile = (path)=>{
                     const renderer = new marked.Renderer();
                     renderer.link = function(href, title, text){
                         links.push({
-                            href:href,
+                            href:href, 
                             text: text,
                             file: path
                         })
@@ -57,8 +57,7 @@ const extractLinksFromFile = (path)=>{
         }
         catch(error){
             reject(error);
-        }
-        
+        }        
     })
 }
 
@@ -68,7 +67,6 @@ PASOS:
 - Se retorna una Promise.all() donde al arreglo de links se le aplica un map, para que posteriormente
 a cada elemento encontrado se le agregue el status y textstatus.
 - Se crea una nueva promesa, que al usar fetch puedo ir agregando a cada link su status y textStatus
-- Con esto, puedo resolver la promesa (resolve) o generar un error (reject)
 */
 
 const validateLink = (links)=>{
@@ -89,18 +87,37 @@ const validateLink = (links)=>{
     }))
 }
 
-/*3)Función statsLinks que permite realizar el calculo de estadística de un archivo*/
+/*
+3)Función statsLinks que permite realizar el calculo de estadística de un archivo
+PASOS:
+**************** 3.1) CASO --stats*******************
+- Sus parámetros son el arreglo de links, y la opción --validate
+- Usando map(), se obtiene el href del arreglo links, guardando esta información en el arreglo hrefLink 
+- Para calcular los links total, se usa length, guardando esta información en el objeto responseStats
+- Para calcular los links unicos, se usa el metodo Set, que permite sacar los elementos sin repetirlos para guardarlos 
+en el arreglo hrefSet.
+- Para calcular los links unicos, se usa size(propiedad de Set), guardando esta información en el objeto responseStats
+- se retorna el objeto responseStats
+**************** 3.2) CASO --stats --validate*******************
+- Si el usuario ingresa la opción --validate, podra obtener los links rotos encontrados.
+- Se usa filter para los links que devuelvan un status igual a 0 o mayor e igual a 400.
+- Se retorna el objeto responseStats con linksTotal, linksUnique y linksBroken
+*/
 
-const statsLinks = (links)=>{
+const statsLinks = (links, options)=>{
     let hrefLink = [];
     let responseStats = {};
     hrefLink = links.map(link=>{
         return link.href;
     });
     responseStats.linksTotal=hrefLink.length;
-
     let hrefSet= new Set(hrefLink);
     responseStats.linksUnique=hrefSet.size;
+    if(options && options.validate){
+        responseStats.linksBroken = links.filter(link=>{
+            return link.status===0 || link.status>=400;
+        }).length;
+    }
     return responseStats;
 }
 
